598b1dfa22915bb8061f04f97dbb26d34ffdf6e3
Violet <vlingenfelter5@gmail.com>
2018-12-14 02:17:18 -0500

    changed to honeydew theme

--- index.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("sidenav").style.width = "250px";
-    document.getElementById("website-content").style.marginLeft = "250px";
-}
-
-function closeNav() {
-    document.getElementById("sidenav").style.width = "0";
-    document.getElementById("website-content").style.marginLeft= "0";
-}
